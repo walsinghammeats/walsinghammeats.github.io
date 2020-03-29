5db4b4c533b8a92f3f0f1e701e4e3ba6a9e76179
walsinghammeats <56370913+walsinghammeats@users.noreply.github.com>
2020-03-29 12:18:43 -0700

    Update data.js

--- src/content/priceByTheCase/data.js
+++ src/content/priceByTheCase/data.js
@@ -1,22 +1,12 @@
 export default {
     "title": "By The Case",
     "items": [
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 },
-        { "title": "40lb Box", "price": 7.99 }
+        { "title": "40lb Box Chicken Leg Qtr", "price": 26.99 },
+        { "title": "40lb Box Chicken Drumsticks", "price": 29.99 },
+        { "title": "40lb Box Chicken Wings", "price": "Call" },
+        { "title": "30lb Box Ribs", "price": 69.99 },
+        { "title": "10lb Box Uncle John Sausage", "price": 36.99 },
+        { "title": "10lb Box", "price": 36.99 }
     ]
 };
 
