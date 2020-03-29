b33a0043795241bfb36c9d769f8d149fe1bd63d4
walsinghammeats <56370913+walsinghammeats@users.noreply.github.com>
2020-03-29 12:26:16 -0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -10,6 +10,7 @@ import Pick5 from './content/pick5/Pick5.js';
 import pick5_data from './content/pick5/data.js';
 import poultry_data from './content/poultry/data.js';
 import priceByTheCase_data from './content/priceByTheCase/data.js';
+import usdaBeefSpecial from './content/priceByTheCase/beef-data.js';
 import SingleColumnPriceTable from './content/common/SingleColumnPriceTable.js';
 import TwoColumnPriceTable from './content/common/TwoColumnPriceTable.js';
 import SingleColumnList from './content/common/SingleColumnList.js';
@@ -31,7 +32,7 @@ class Home extends Component {
                 </div>
                 <div class="w3-content">
                     <div class="w3-row-padding card-deck">
-                        <${SingleColumnPriceTable} ...${priceByTheCase_data} />
+                        <${SingleColumnPriceTable} ...${usdaBeefSpecial} />
                         <${TwoColumnPriceTable}  ...${poultry_data} />
                     </div>
                 </div>
