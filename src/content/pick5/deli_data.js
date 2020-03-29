c32a0450e1e0c68c26abc493c86d0a69bdafab37
walsinghammeats <56370913+walsinghammeats@users.noreply.github.com>
2020-03-29 18:00:17 -0400

    Update deli_data.js

--- src/content/pick5/deli_data.js
+++ src/content/pick5/deli_data.js
@@ -1,8 +1,12 @@
 export default {
     "title": "Pick Any 5 Items",
-    "price": "19.99",
+    "discountPrice": "24.99",
     "items": [
-        "1lb Bacon",
-        "1.75lb Andy Sausage"
+        "1lb Roast Beef",
+        "1lb Turkey Breast",
+        "1lb Ham",
+        "1lb Bologna",
+        "1lb Salami",
+        "1lb American Cheese"
     ]
 };
