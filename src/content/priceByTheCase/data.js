48eeec733dc81903cefcd43becf3da6ecd65dd3c
walsinghammeats <56370913+walsinghammeats@users.noreply.github.com>
2020-03-29 18:02:50 -0400

    Update data.js

--- src/content/priceByTheCase/data.js
+++ src/content/priceByTheCase/data.js
@@ -3,7 +3,7 @@ export default {
     "items": [
         { "title": "40lb Box Chicken Leg Qtr", "price": 26.99 },
         { "title": "40lb Box Chicken Drumsticks", "price": 29.99 },
-        { "title": "40lb Box Chicken Wings", "price": "Call" },
+        { "title": "40lb Box Chicken Wings", "price": "...Call..." },
         { "title": "30lb Box Ribs", "price": 69.99 },
         { "title": "10lb Box Uncle John Sausage", "price": 36.99 },
         { "title": "10lb Box Alabama Sausage", "price": 36.99 }
