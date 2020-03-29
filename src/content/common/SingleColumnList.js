e2c328ad2adf6c980e3444b1fc3e85f8f64ceec2
walsinghammeats <56370913+walsinghammeats@users.noreply.github.com>
2020-03-29 17:58:26 -0400

    Update SingleColumnList.js

--- src/content/common/SingleColumnList.js
+++ src/content/common/SingleColumnList.js
@@ -10,7 +10,6 @@ const TwoColumnList = props => html`
           </div>
           <div class="w3-container w3-medium">
             <p class="w3-center w3-padding">
-              <span class="w3-medium w3-opacity"><del>$${props.price}</del></span>
               <span class="w3-badge w3-padding w3-large w3-red w3-animate-zoom"><b>$${props.discountPrice}</b></span>
             </p>
             <div class="w3-row-padding">
