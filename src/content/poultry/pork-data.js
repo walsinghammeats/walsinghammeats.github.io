d6f7ceac863c39fb54419dda83147ab437060958
walsinghammeats <56370913+walsinghammeats@users.noreply.github.com>
2020-11-30 08:29:53 -0500

    Update pork-data.js
    updated pork special

--- src/content/poultry/pork-data.js
+++ src/content/poultry/pork-data.js
@@ -5,9 +5,9 @@ export default {
         { "title": "Pig Feet", "five": 7.99, "ten": 13.99 },
         { "title": "Rib Tips", "five": 7.99, "ten": 14.99 },
         { "title": "5lb Clean Chittlening", "five": "N/A", "ten": 10.99 },
-        { "title": "5lb Uncle Johns Sausage", "five": "N/A", "ten": 19.99 },
-        { "title": "5lb Smoked Bacon", "five": "N/A", "ten": 19.99 },
-        { "title": "5lb Center Cut Pork Chops", "five": "N/A", "ten": 12.99 },
-        { "title": "5lb End Cut Pork Chops", "five": "N/A", "ten": 10.99 }
+        { "title": "5lb Uncle Johns Sausage", "five": "N/A", "ten": 22.99 },
+        { "title": "5lb Smoked Bacon", "five": "N/A", "ten": 22.99 },
+        { "title": "5lb Center Cut Pork Chops", "five": "N/A", "ten": 11.99 },
+        { "title": "5lb End Cut Pork Chops", "five": "N/A", "ten": 9.99 }
     ]
 };
