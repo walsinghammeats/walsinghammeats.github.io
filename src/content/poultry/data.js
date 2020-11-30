140899c809ef278b82d9c20213e3732f1c759ff4
walsinghammeats <56370913+walsinghammeats@users.noreply.github.com>
2020-11-30 08:28:57 -0500

    Update data.js
    updated poultry prices

--- src/content/poultry/data.js
+++ src/content/poultry/data.js
@@ -4,7 +4,7 @@ export default {
         { "title": "Chicken Leg Qtr", "five": 4.99, "ten": 8.99 },
         { "title": "Chicken Wings", "five": 13.99, "ten": 25.99 },
         { "title": "Chicken Drumsticks", "five": 5.99, "ten": 9.99 },
-        { "title": "Turkey Wings", "five": 7.99, "ten": 13.99 },
+        { "title": "Turkey Wings", "five": 8.99, "ten": 15.99 },
         { "title": "Turkey Necks", "five": 6.99, "ten": 12.99 },
         { "title": "B/S Chicken Breast", "five": 12.99, "ten": 23.99 }
     ]
