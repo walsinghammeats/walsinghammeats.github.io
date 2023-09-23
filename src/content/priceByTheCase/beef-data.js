ba137e17814eb93b402afb2607e9e01785cc30bc
muadhaddabah <55167706+muadhaddabah@users.noreply.github.com>
2023-09-22 23:42:30 -0400

    Update beef-data.js updated beef-data prices

--- src/content/priceByTheCase/beef-data.js
+++ src/content/priceByTheCase/beef-data.js
@@ -2,15 +2,15 @@ export default {
     "title": "USDA Beef Special",
     "items": [
         { "title": "5lb T-Bone Steak", "price": 35.99 },
-        { "title": "5lb Rib Eye", "price": 39.99 },
-        { "title": "Whole Brisket (per lb)", "price": 3.99 },
-        { "title": "1lb Filet Mignon", "price": 9.99 },
-        { "title": "1lb Oxtail", "price": 8.99 },
-        { "title": "1lb Chuck Roast", "price": 3.99 },
-        { "title": "1lb Beef Stew", "price": 4.99 },
-        { "title": "5lb Ground Beef", "price": 15.99 },
-        { "title": "5lb Cube Steak", "price": 27.99 },
-        { "title": "5lb Beef Short Ribs", "price": 31.99 },
+        { "title": "Rib Eye", "price": 9.99 },
+        { "title": "Whole Brisket (per lb)", "price": 5.99 },
+        { "title": "1lb Filet Mignon", "price": 12.99 },
+        { "title": "1lb Oxtail", "price": 9.99 },
+        { "title": "1lb Chuck Roast", "price": 4.99 },
+        { "title": "1lb Beef Stew", "price": 5.99 },
+        { "title": "5lb Ground Beef", "price": 16.99 },
+        { "title": "5lb Cube Steak", "price": 35.99 },
+        { "title": "5lb Beef Short Ribs", "price": 37.99 },
         { "title": "5lb New York Strip", "price": 39.99 }
     ]
 };
