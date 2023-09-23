3efafc31a7dc1dc1cba81bd2776ec5b2a66098eb
muadhaddabah <55167706+muadhaddabah@users.noreply.github.com>
2023-09-22 23:35:48 -0400

    Update data.js poultry prices

--- src/content/poultry/data.js
+++ src/content/poultry/data.js
@@ -1,11 +1,11 @@
 export default {
     "title": "Poultry Special",
     "items": [
-        { "title": "Chicken Leg Qtr", "five": 4.99, "ten": 8.99 },
-        { "title": "Chicken Wings", "five": 13.99, "ten": 25.99 },
+        { "title": "Chicken Leg Qtr", "five": 5.99, "ten": 9.99 },
+        { "title": "Chicken Wings", "five": 14.99, "ten": 27.99 },
         { "title": "Chicken Drumsticks", "five": 5.99, "ten": 9.99 },
-        { "title": "Turkey Wings", "five": 8.99, "ten": 15.99 },
-        { "title": "Turkey Necks", "five": 6.99, "ten": 12.99 },
-        { "title": "B/S Chicken Breast", "five": 12.99, "ten": 23.99 }
+        { "title": "Turkey Wings", "five": 11.99, "ten": 22.99 },
+        { "title": "Turkey Necks", "five": 8.99, "ten": 16.99 },
+        { "title": "B/S Chicken Breast", "five": 14.99, "ten": 28.99 }
     ]
 };
